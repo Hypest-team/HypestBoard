049bb1498df4b212bf1dfb2c08e0dcaa42067f6a
David Serrano <david.ma.serrano@gmail.com>
2018-02-17 19:31:58 +0100

    Added default route to GET / to point to an index.html, that should be used to mange the scoreboard

--- api/routes/scoreboardRoutes.js
+++ api/routes/scoreboardRoutes.js
@@ -3,12 +3,22 @@ var path = require('path');
 module.exports = function(app) {
 	var scoreboard = require('../controllers/scoreboardController')
 
+    function getStaticFilePath(file) {
+        return path.resolve(__dirname + '../../../Stuff/Content/html' + file);
+    }
+
 	app.route('/scoreboard')
 		.get(scoreboard.getScoreboard)
 		.post(scoreboard.updateScoreboard);
 
+    app.get('/', function (req, res) {
+        var filePath = getStaticFilePath('/index.html');
+        console.log('Sending static file', filePath);
+        res.sendFile(filePath);
+    });
+
 	app.get(/^(.+)$/, function (req, res) {
-		var filePath = path.resolve(__dirname + '../../../Stuff/Content/html' + req.params[0]);
+        var filePath = getStaticFilePath(req.params[0]);
 		console.log('Sending static file', filePath);	
 		res.sendFile(filePath);
 	});
