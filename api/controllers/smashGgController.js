798e59574c35a8a335c7efefd72d7def659b47c4
David Serrano <dserrano@linkare.com>
2018-04-29 18:59:15 +0200

    Added basic support for stream queue listing.

--- api/controllers/smashGgController.js
+++ api/controllers/smashGgController.js
@@ -1,4 +1,5 @@
 var fetch = require('node-fetch');
+var _ = require('lodash');
 var serverUrl = 'https://api.smash.gg/';
 
 function getFullUrl(call) {
@@ -16,18 +17,64 @@ function processResponse(response) {
 function getRequest(path, res) {
     var url = getFullUrl(path);
 
-    fetch(url)
+    console.log('Fetching', url);
+
+    return fetch(url)
         .then(processResponse)
-        .then(function (data) {
-            res.json(data);
-            return data;
+        .catch(function (e) {
+            console.log('SmashGG API error', e);
         });
 }
 
+function enhanceStationQueue(stationQ) {
+    if (!stationQ.queues.length) {
+        return stationQ;
+    }
+
+    var sets = stationQ.data.entities.sets;
+    var entrants = stationQ.data.entities.entrants;
+    var player = stationQ.data.entities.player;
+
+    // Fill in player data
+    stationQ.data.entities.sets = sets.map(function (set) {
+        var newSet = _.cloneDeep(set);
+
+        var entrantIdKeys = Object.keys(set).filter(function (key) {
+            return /entrant\dId$/.test(key);
+        });
+       
+        newSet.entrants = [];
+
+        entrantIdKeys.forEach(function (entrantIdKey) {
+            var entrantKey = entrantIdKey.replace('Id', '');
+            var entrant = entrants.find(function (entrant) {
+                return entrant.id === set[entrantIdKey];
+            });
+
+            if (entrant) {
+                newSet.entrants.push(entrant);
+            }
+        });
+
+        return newSet;
+    });
+
+    return stationQ;
+}
+
 exports.getTournament = function (req, res) {
-    return getRequest('tournament/' + req.params.tournamentSlug, res);
+    return getRequest('tournament/' + req.params.tournamentSlug, res)
+        .then(function (data) {
+            res.json(data);
+        });
 };
 
 exports.getStationQueue = function (req, res) {
-    return getRequest('station_queue/' + req.params.tournamentId, res);
+    return getRequest('station_queue/' + req.params.tournamentId, res)
+        .then(function (data) {
+            res.json(enhanceStationQueue(data));
+        })
+        .catch(function (e) {
+            console.error(e);
+        });
 };
