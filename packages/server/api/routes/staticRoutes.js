4b32c029b41be05e541fc9a0a19127532b0bad3b
David Serrano <dserrano@linkare.com>
2020-05-20 16:03:15 +0200

    #3 initial work on external overlay availability

--- packages/server/api/routes/staticRoutes.js
+++ packages/server/api/routes/staticRoutes.js
@@ -2,12 +2,27 @@ const clientPath = require.resolve('@scoreman/client');
 const overlays = require('@scoreman/overlays');
 
 const path = require('path');
-const serveStatic = require('serve-static');
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const EXT_OVERLAY_PATHNAME = 'overlays';
 
 module.exports = function (app) {
-    app.use('/api/config', serveStatic(path.resolve(__dirname, '../../data/config')));
-    app.use('/api/characters', serveStatic(path.resolve(__dirname, '../../data/characters')));
+    const basePath = path.dirname(require.main.filename);
+    const staticPath = path.dirname(clientPath);
 
-    app.use('/', serveStatic(path.dirname(clientPath)));
-    app.use('/overlays', serveStatic(overlays.root));
+    app.use('/api/config', express.static(
+        path.resolve(basePath, 'data', 'config')
+    ));
+    app.use('/api/characters', express.static(
+        path.resolve(basePath, 'data', 'characters')
+    ));
+
+    app.use('/', express.static(staticPath));
+
+    app.use('/overlays', serveIndex(path.resolve(basePath, EXT_OVERLAY_PATHNAME)));
+    app.use('/overlays', serveIndex(overlays.root));
+
+    app.use('/overlays', express.static(path.resolve(basePath, EXT_OVERLAY_PATHNAME)));
+    app.use('/overlays', express.static(overlays.root));
 };
