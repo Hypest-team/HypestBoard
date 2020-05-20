b8c26a4a1e393cf3aa366b22855977d1a5941d1b
David Serrano <dserrano@linkare.com>
2020-05-20 16:34:27 +0200

    Added possiblity to read installed scoreman-overlay npm modules

--- packages/server/api/routes/staticRoutes.js
+++ packages/server/api/routes/staticRoutes.js
@@ -3,14 +3,44 @@ const overlays = require('@scoreman/overlays');
 
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
 const serveIndex = require('serve-index');
 
 const EXT_OVERLAY_PATHNAME = 'overlays';
 
-module.exports = function (app) {
-    const basePath = path.dirname(require.main.filename);
-    const staticPath = path.dirname(clientPath);
+const basePath = path.dirname(require.main.filename);
+const staticPath = path.dirname(clientPath);
 
+const npmOverlayPaths = require.resolve.paths('').reduce((acc, npmDir) => {
+    try {
+        const overlayPkgs = fs.readdirSync(npmDir).filter((npmDirPkgs) => {
+            return npmDirPkgs.startsWith('scoreman-overlay');
+        })
+            .map((overlayPkg) => {
+                return path.resolve(npmDir, overlayPkg);
+            });
+
+        overlayPkgs.forEach((overlayPath) => {
+            acc.push(overlayPath);
+        });
+    } catch (e) {
+
+    }
+
+    return acc;
+}, []);
+
+const overlayPaths = [
+    overlays.root,
+    path.resolve(basePath, EXT_OVERLAY_PATHNAME),
+    ...npmOverlayPaths
+]
+
+function overlayManifest(req, res) {
+
+}
+
+module.exports = function (app) {
     app.use('/api/config', express.static(
         path.resolve(basePath, 'data', 'config')
     ));
@@ -20,9 +50,11 @@ module.exports = function (app) {
 
     app.use('/', express.static(staticPath));
 
-    app.use('/overlays', serveIndex(path.resolve(basePath, EXT_OVERLAY_PATHNAME)));
-    app.use('/overlays', serveIndex(overlays.root));
+    app.use('/overlays/manifest.json', overlayManifest);
 
-    app.use('/overlays', express.static(path.resolve(basePath, EXT_OVERLAY_PATHNAME)));
-    app.use('/overlays', express.static(overlays.root));
+    overlayPaths.forEach((overlayDir) => {
+        app.use('/overlays', serveIndex(overlayDir));
+        app.use('/overlays', express.static(overlayDir));
+    });
+
 };
