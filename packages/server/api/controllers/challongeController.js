8cf86fed790ba31ed23e1ddeecb17b77dc68ced9
David Serrano <dserrano@linkare.com>
2020-05-21 13:53:52 +0200

    #7 better challonge API handling

--- packages/server/api/controllers/challongeController.js
+++ packages/server/api/controllers/challongeController.js
@@ -1,33 +1,31 @@
 const axios = require('axios').default;
 const baseUrl = 'https://api.challonge.com/v1/';
 
-function get(call, apikey) {
+function get(call, res, apikey) {
     const url = `${baseUrl}${call}?api_key=${apikey}`;
 
-    console.log('getting', url);
-
-    return axios.get(url);
+    console.log('[Challonge API call]', url);
+    return axios.get(url)
+        .then(({data}) => res.json(data))
+        .catch(console.error);
 }
 
 function getTournament(req, res) {
     const { apikey } = req.query;
     const { tournamentId } = req.params;
-    get(`tournaments/${tournamentId}.json`, apikey)
-        .then(res.json);
+    get(`tournaments/${tournamentId}.json`, res, apikey);
 }
 
 function getParticipant(req, res) {
     const { apikey } = req.query;
     const { tournamentId, participantId } = req.params;
-    get(`tournaments/${tournamentId}/participants/${participantId}.json`, apikey)
-        .then(res.json)
+    get(`tournaments/${tournamentId}/participants/${participantId}.json`, res, apikey);
 }
 
 function getMatches(req, res) {
     const { apikey } = req.query;
     const { tournamentId } = req.params;
-    get(`tournaments/${tournamentId}/matches`, apikey)
-        .then(res.json)
+    get(`tournaments/${tournamentId}/matches.json`, res, apikey);
 }
 
 module.exports = {
