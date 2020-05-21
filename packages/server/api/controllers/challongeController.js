acaa28c2883858e7ffdce051bb7d6e444fa40a45
David Serrano <dserrano@linkare.com>
2020-05-21 14:27:48 +0200

    #7 matches are now ehanced with player data

--- packages/server/api/controllers/challongeController.js
+++ packages/server/api/controllers/challongeController.js
@@ -2,12 +2,16 @@ const axios = require('axios').default;
 const baseUrl = 'https://api.challonge.com/v1/';
 
 function get(call, res, apikey) {
+    return getApi(call, apikey)
+        .then(({ data }) => res.json(data))
+        .catch(console.error);
+}
+
+function getApi(call, apikey) {
     const url = `${baseUrl}${call}?api_key=${apikey}`;
 
     console.log('[Challonge API call]', url);
-    return axios.get(url)
-        .then(({data}) => res.json(data))
-        .catch(console.error);
+    return axios.get(url);
 }
 
 function getTournament(req, res) {
@@ -16,20 +20,52 @@ function getTournament(req, res) {
     get(`tournaments/${tournamentId}.json`, res, apikey);
 }
 
-function getParticipant(req, res) {
-    const { apikey } = req.query;
-    const { tournamentId, participantId } = req.params;
-    get(`tournaments/${tournamentId}/participants/${participantId}.json`, res, apikey);
+function resolvePlayer(tournamentId, playerId, apikey) {
+    return getApi(`tournaments/${tournamentId}/participants/${playerId}.json`, apikey)
+        .then(({ data }) => {
+            return data.participant;
+        });
+}
+
+function enhanceMatches(matches, tournamentId, apikey) {
+    const promises = matches.map(({ match }) => {
+        return Promise.all([
+            resolvePlayer(tournamentId, match.player1_id, apikey),
+            resolvePlayer(tournamentId, match.player2_id, apikey)
+        ])
+            .then(([player1, player2]) => {
+                return {
+                    player1,
+                    player2,
+                    ...match
+                };
+            })
+    });
+
+    return Promise.all(promises)
+        .then((matches) => {
+            //console.log('all matches', matches);
+            return matches;
+        })
+        .catch((err) => {
+            console.err('enahcne all er', err);
+        });
 }
 
 function getMatches(req, res) {
     const { apikey } = req.query;
     const { tournamentId } = req.params;
-    get(`tournaments/${tournamentId}/matches.json`, res, apikey);
+    getApi(`tournaments/${tournamentId}/matches.json`, apikey)
+        .then(({ data }) => {
+            return enhanceMatches(data, tournamentId, apikey);
+        })
+        .then((result) => {
+            res.json(result);
+            return result;
+        });
 }
 
 module.exports = {
     getTournament,
-    getParticipant,
     getMatches
 };
